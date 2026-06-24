4ba8ec5a9879c3971f6a11951aa39db54492365c
matteopinfildi <pinfildimatteo@gmail.com>
2026-06-24 12:06:10 +0200

    Modifica test CF per ResourceLogic

--- core/idm/logic/src/test/java/org/apache/syncope/core/logic/ResourceLogicCFTest.java
+++ core/idm/logic/src/test/java/org/apache/syncope/core/logic/ResourceLogicCFTest.java
@@ -17,6 +17,7 @@ import org.identityconnectors.framework.common.objects.ConnectorObject;
 import org.identityconnectors.framework.common.objects.ResultsHandler;
 import org.identityconnectors.framework.common.objects.SearchResult;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -71,11 +72,12 @@ public class ResourceLogicCFTest {
     // searchConnObjects
 
     @Test(expected = NotFoundException.class)
-    public void testSearchConn_TC08() {
+    public void testSearchConn_TC010() {
         /*
         Category partition:
-        - RealmType = REALM_ANYTYPE
-        - OrgUnit = null
+        A1 = Key valida
+        B4 = REALM_ANYTYPE
+        C1 = size valido
 
 
          Oracolo: Il flusso deve essere interrotto per assenza di regole di provisioning --> NotFoundException
@@ -84,48 +86,10 @@ public class ResourceLogicCFTest {
         String realmAnyType = SyncopeConstants.REALM_ANYTYPE;
         ExternalResource mockResource = mock(ExternalResource.class);
 
-        doReturn(Optional.of(mockResource)).when(resourceDAO).findById("DB-HR");
+        doReturn(Optional.of(mockResource)).when(resourceDAO).findById("Resource_DB");
         when(mockResource.getOrgUnit()).thenReturn(null);
 
-        resourceLogic.searchConnObjects(null, Collections.emptySet(), "DB-HR", realmAnyType, 10, null, Collections.emptyList());
+        resourceLogic.searchConnObjects(null, Collections.emptySet(), "Resource_DB", realmAnyType, 10, null, Collections.emptyList());
     }
 
-    @Test
-    public void testSearchConn_TC09() {
-        /*
-         Category partition:
-        - RealmType = REALM_ANYTYPE
-        - OrgUnit = presente e valida
-        - Connector Search = Simulazione callback riuscita
-
-        Oracolo: Il blocco anonimo interno deve essere eseguito correttamente,popolando la lista di ritorno.
-        */
-
-        String realmAnyType = SyncopeConstants.REALM_ANYTYPE;
-        ExternalResource mockResource = mock(ExternalResource.class);
-        doReturn(Optional.of(mockResource)).when(resourceDAO).findById("DB-HR");
-
-        OrgUnit mockOrgUnit = mock(OrgUnit.class);
-        when(mockResource.getOrgUnit()).thenReturn(mockOrgUnit);
-        when(mockOrgUnit.getObjectClass()).thenReturn("__REALM__");
-        when(mockOrgUnit.getItems()).thenReturn(Collections.emptyList());
-
-        Connector mockConnector = mock(Connector.class);
-        when(connectorManager.getConnector(mockResource)).thenReturn(mockConnector);
-
-        doAnswer(invocation -> {
-            ResultsHandler handler = invocation.getArgument(2);
-            ConnectorObject mockConnObj = mock(ConnectorObject.class);
-            when(mockConnObj.getAttributes()).thenReturn(Collections.emptySet());
-
-            handler.handle(mockConnObj);
-            return null;
-        }).when(mockConnector).search(any(), any(), any(), anyInt(), any(), any(), any());
-
-        Pair<SearchResult, List<ConnObject>> result = resourceLogic.searchConnObjects(
-                null, Collections.emptySet(), "DB-HR", realmAnyType, 10, null, Collections.emptyList());
-
-        assertNotNull("La pair restituita, che contiene SearchResult e una lista di ConnObjects non deve essere nulla", result);
-        assertEquals("L'handler avrebbe dovuto aggiungere 1 elemento all'elenco", 1, result.getRight().size());
-    }
 }
