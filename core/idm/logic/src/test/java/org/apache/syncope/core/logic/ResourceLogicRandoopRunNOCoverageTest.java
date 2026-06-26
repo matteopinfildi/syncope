cfa217ac09242117e821e531720fc5fb2bede587
matteopinfildi <pinfildimatteo@gmail.com>
2026-06-26 20:05:19 +0200

    Aggiunta test ES per entrambe le classi

---
+++ core/idm/logic/src/test/java/org/apache/syncope/core/logic/ResourceLogicRandoopRunNOCoverageTest.java
@@ -0,0 +1,10 @@
+package org.apache.syncope.core.logic;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ ResourceLogicRandoopNOCoverageTest.class })
+public class ResourceLogicRandoopRunNOCoverageTest {
+}
+
